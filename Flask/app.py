7b19159ff9aed6d6c4564260f75431bfe578ea0e
Guilherme Bovi Ambrosano <guibovi2@gmail.com>
2020-03-22 15:43:06 -0300

    Agora o Flask+Bokeh tá funcionando

--- Flask/app.py
+++ Flask/app.py
@@ -3,6 +3,7 @@ import urllib
 
 from bokeh.plotting import figure
 from bokeh.embed import components
+from bokeh.io import show
 
 from flask import Flask, render_template
 
@@ -13,7 +14,8 @@ file_name, headers = urllib.request.urlretrieve(link)
 df = pd.read_excel(file_name)
 
 p = figure(plot_width=800, plot_height=250, x_axis_type="datetime")
-p.line(df["DateRep"], df["Deaths"], color="navy", alpha=0.5)
+p.circle(df["DateRep"], df["Deaths"], color="navy", alpha=0.5)
+# show(p)
 
 script, div = components(p)
 
@@ -23,3 +25,7 @@ app = Flask("__name__")
 @app.route("/")
 def main():
     return render_template("index.html", script=script, div=div)
+
+
+if __name__ == "__main__":
+    app.run()
