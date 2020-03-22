b05c1e08ff85c74582417a022d1ee04e50bf45be
Guilherme Bovi Ambrosano <guibovi2@gmail.com>
2020-03-22 01:45:19 -0300

    Criei a pasta Flask pra testar

---
+++ Flask/app.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import urllib
+
+from bokeh.plotting import figure
+from bokeh.embed import components
+
+from flask import Flask, render_template
+
+
+link = "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-2020-03" \
+       "-21.xlsx"
+file_name, headers = urllib.request.urlretrieve(link)
+df = pd.read_excel(file_name)
+
+p = figure(plot_width=800, plot_height=250, x_axis_type="datetime")
+p.line(df["DateRep"], df["Deaths"], color="navy", alpha=0.5)
+
+script, div = components(p)
+
+app = Flask("__name__")
+
+
+@app.route("/")
+def main():
+    return render_template("index.html", script=script, div=div)
