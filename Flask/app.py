dabafbf73d4dc3a121460b8e7ffc2046d57b449b
Gui <guibovi2@gmail.com>
2020-04-09 23:37:08 -0300

    Adicionei um mapa no site e to tentando fazer o geopy funcionar de novo

--- Flask/app.py
+++ Flask/app.py
@@ -15,7 +15,8 @@ from geopy.extra.rate_limiter import RateLimiter
 
 from bokeh.plotting import figure
 from bokeh.embed import components
-from bokeh.models import DatetimeTickFormatter
+from bokeh.models import DatetimeTickFormatter, WheelZoomTool
+from bokeh.tile_providers import CARTODBPOSITRON, get_provider
 from bokeh.io import show
 
 from datetime import date, timedelta
@@ -34,6 +35,7 @@ app = Flask("__name__")
 
 # Configurando o banco de dados
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# Colocar o caminho pra database nas variáveis do ambiente (e.g.: DATABASE_URL = /database.db)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
 db = SQLAlchemy(app)
 
@@ -44,7 +46,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configurando o geopy
-geolocator = Nominatim(user_agent="corona")
+geolocator = Nominatim(user_agent="corona", timeout=10)
 geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
 
 
@@ -63,7 +65,7 @@ def atualizar_dados():
 
     ontem_str = ontem.strftime("%Y-%m-%d") + ".xlsx"
     arquivos_excel = [arq for arq in os.listdir(".") if arq.endswith(".xlsx")]
-    if ontem_str not in arquivos_excel:
+    if ontem_str not in arquivos_excel or "paises_" + ontem_str not in arquivos_excel:
         # Baixando a planilha da internet e carregando no pandas
         link = "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-"+\
                ontem_str
@@ -86,17 +88,26 @@ def atualizar_dados():
         df.to_excel(ontem_str)
 
         # Pegando as coordenadas dos países
-        # paises = atualizar_localizacao(df)
-        # paises.to_excel("paises_" + ontem_str)
-        paises = None
+        paises = atualizar_localizacao(df)
+        paises.to_excel("paises_" + ontem_str)
 
     else:
         # Se já tiver planilha, é só ler
         df = pd.read_excel(ontem_str)
-        # paises = pd.read_excel("paises_" + ontem_str)
-        paises = None
+        paises = pd.read_excel("paises_" + ontem_str)
 
     return paises, df
+
+
+def geocode_print(x):
+    print(x)
+    try:
+        geo = geocode(x)
+        print("deu\n")
+    except:
+        print("não deu\n")
+        geo = {"point": (None, None)}
+    return geo
 
 
 def atualizar_localizacao(df):
@@ -107,8 +118,10 @@ def atualizar_localizacao(df):
     paises.index.name = "countriesAndTerritories"
     paises.reset_index(inplace=True)
 
-    paises["location"] = paises["countriesAndTerritories"].replace("_", " ", regex=True).apply(geocode)
+    paises["location"] = paises["countriesAndTerritories"].replace("_", " ", regex=True).apply(geocode_print)
+    print(paises.head())
     paises["point"] = paises["location"].apply(lambda loc: tuple(loc.point) if loc else None)
+    print(paises.head())
     return paises
 
 
@@ -119,6 +132,7 @@ def atualizar_grafico_aumento(pais=None, df=None):
     p = figure(plot_width=800, plot_height=250, x_axis_type="datetime",
                x_axis_label="Data",
                y_axis_label="Casos acumulados")
+    p.toolbar.active_scroll = p.select_one(WheelZoomTool)
     # Criando coluna casesCumulative, com o número cumulativo de casos (começando no primeiro dia, indo até o último)
     df["casesCumulative"] = df.iloc[::-1].groupby("countriesAndTerritories").cases.cumsum()[::-1]
     if pais is None:
@@ -141,6 +155,7 @@ def atualizar_grafico_expon(pais=None, df=None):
     p = figure(plot_width=800, plot_height=250, y_axis_type="log", x_axis_type="log",
                x_axis_label="Diferença no número de casos da semana anterior",
                y_axis_label="Casos acumulados por semana")
+    p.toolbar.active_scroll = p.select_one(WheelZoomTool)
     df = df.groupby(["week", "countriesAndTerritories"]).sum().reset_index()
     df["casesDiff"] = df.groupby("countriesAndTerritories").casesCumulative.diff()
     if pais is None:
@@ -156,6 +171,27 @@ def atualizar_grafico_expon(pais=None, df=None):
     script, div = components(p)
     return script, div
 
+
+def atualizar_grafico_mapa(paises=None):
+    if paises is None:
+        paises, df = atualizar_dados()
+
+    paises["x"] = paises.point.apply(lambda x: x[0])
+    paises["y"] = paises.point.apply(lambda x: x[1])
+    tile_provider = get_provider(CARTODBPOSITRON)
+    p = figure(x_range=(-2000000, 6000000), y_range=(-1000000, 7000000),
+               x_axis_type="mercator", y_axis_type="mercator")
+    p.toolbar.active_scroll = p.select_one(WheelZoomTool)
+    p.add_tile(tile_provider)
+    p.circle(x=paises.x,
+             y=paises.y,
+             size=paises.cases,
+             line_color="navy",
+             fill_color="navy",
+             fill_alpha=0.05)
+
+    script, div = components(p)
+    return script, div
 
 @app.route("/")
 def main():
@@ -174,7 +210,10 @@ def casos():
         pais = pais.replace(" ", "_")
 
     paises, df = atualizar_dados()
-    paises = df["countriesAndTerritories"].replace("_", " ", regex=True).unique()
+
+    script, div = atualizar_grafico_mapa(df=df, paises=paises)
+    divs.append(div)
+    scripts.append(script)
 
     script, div = atualizar_grafico_aumento(pais=pais, df=df)
     divs.append(div)
@@ -187,6 +226,7 @@ def casos():
     ultima_atualizacao = date.today() - timedelta(days=1)
     data = ultima_atualizacao.strftime("%d/%m/%Y")
 
+    paises = df["countriesAndTerritories"].replace("_", " ", regex=True).unique()
     return render_template("casos.html", scripts=scripts, divs=divs, data=data,
                            paises=paises, selecionado=selecionado,
                            route="casos")
